97c00119897d83fa61d53f60521dbefc0cd4fb03
Marcus Casswell <mcaswell2@gmail.com>
2019-04-08 21:31:17 -0400

    new set up

--- bot.js
+++ bot.js
@@ -23,7 +23,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+        var name = args[2];
+        
         args = args.splice(1);
         switch(cmd) {
             // !ping
@@ -31,6 +32,25 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
+                });
+            break;
+            case 'rank':
+//                var players = {
+//                    method: 'GET',
+//                    url: 'https://na1.api.riotgames.com/lol/',
+//                    headers:
+//                    {
+//                        'Postman-Token': '2efa7b32-d53c-4897-812e-c685ce7047a9',
+//                        'cache-control': 'no-cache',
+//                        Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjYjBkY2IxMC1mNDI0LTAxMzYtMGNjMi0xOWQxZThiMmYzYzAiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTQ2ODA4NzYwLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InJpdmFzLWJvdCJ9.5iE0fPD1qpZD25BQozUmjY3PxiVP3snH3e9Cvb87wjI',
+//                        Accept: 'application/json',
+//                        'Content-Type': 'application/vnd.api+json',
+//                        'TRN-API-KEY': '6af93314-54a9-43e1-b63b-8b0b2e32aa85'
+//                    }
+//                };
+                bot.sendMessage({
+                   to: channelID,
+                   message: 'This is your current ranking' + ' ' + args
                 });
             break;
             // Just add any case commands if you want to..
