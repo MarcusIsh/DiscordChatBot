7e49fb67c48aa09e6c425958e773108dd3bb80d0
Marcus Casswell <mcaswell2@gmail.com>
2019-04-08 20:01:46 -0400

    adding working adding account lookup support

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
+logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
