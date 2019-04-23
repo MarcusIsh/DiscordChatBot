8b83e5264261770ee498fddb973a323a4108c43b
Marcus Casswell <mcaswell2@gmail.com>
2019-04-23 02:04:06 -0400

    removing uneeded file

--- bot.js
+++ bot.js
@@ -114,9 +114,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'commands':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'commands\n\
-                             !rank <username> -- shows users rank, adds userdata to database to be tracked\n\
-                             !csrRank -- shows tracked users ranking. INFO: Points are added from tier(0 - 80, by 10s. 0 unranked, challenger 80) and rank(0 - 40, by 10s. 0 unranked, IV 40)'
+                    message: 'commands:\n\
+!rank <username> -- shows users rank, adds userdata to database to be tracked\n\
+!csrRank -- shows tracked users ranking. INFO: Points are added from tier(0 - 80, by 10s. 0 unranked, challenger 80) and rank(0 - 40, by 10s. 0 unranked, IV 40)'
                 });
         }
     }
